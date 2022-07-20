26b595714f6a12ef4901652e7ec1377fb9a218af
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-20 10:12:51 +0000

    initial commit

---
+++ restaurant-website/src/app/display-page/display-page.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable, Subscription, tap } from 'rxjs';
+import { FirebaseFood, OrderDetailsAdmin } from '../models/interface';
+import { OrderType } from '../single-order/single-order.component';
+
+interface Order {
+  // foodName: string;
+  name: string;
+  phoneNumber: string;
+  location: string;
+  extraComments?: string;
+}
+
+@Component({
+  selector: 'app-display-page',
+  templateUrl: './display-page.component.html',
+  styleUrls: ['./display-page.component.scss'],
+})
+export class DisplayPageComponent implements OnInit {
+  item$: Observable<OrderDetailsAdmin[]>;
+  food$: Observable<FirebaseFood[]>;
+  OrderType = OrderType;
+  notificationAudio = new Audio('../../assets/Short-notification-sound.mp3');
+  isFirstTime = true;
+  itemLength: number = 0;
+  subscriptions: Subscription[] = [];
+  constructor(private firestore: AngularFirestore) {
+    this.item$ = this.exampleGetCollection();
+    this.food$ = this.onGetAllFoods();
+    let itemSubs = this.item$.subscribe((res) => {
+      if (!this.isFirstTime && res.length > this.itemLength)
+        this.notificationAudio.play();
+      else this.isFirstTime = false;
+
+      this.itemLength = res.length;
+    });
+
+    this.subscriptions.push(itemSubs);
+  }
+
+  success: boolean = false;
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscriptions.forEach((sub) => sub?.unsubscribe());
+  }
+
+  exampleGetCollection(): Observable<any> {
+    return this.firestore
+      .collection('orders', (orders) =>
+        orders
+          .where('completed', '==', false)
+          .where('orderPaid', '==', true)
+          .orderBy('date', 'desc')
+      )
+      .valueChanges({ idField: 'Id' });
+  }
+
+  onOrderDelivered(id: string, orderId: string): void {
+    if (window.confirm(`Are you sure you want to comfirm oder: ${orderId}?`)) {
+      this.updateOrder(id, { completed: true })
+        // .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+      this.success = true;
+    }
+  }
+
+  onCancelOrder(id: string, orderId: string) {
+    if (window.confirm(`Do you really want to delete oder: ${orderId}?`)) {
+      this.deleteOrder(id)
+        // .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+  }
+
+  updateOrder(id: string, data: { completed: boolean }): Promise<void> {
+    return this.firestore.collection('orders').doc(id).update(data);
+  }
+
+  deleteOrder(id: string): Promise<void> {
+    return this.firestore.collection('orders').doc(id).delete();
+  }
+  onGetAllFoods(): Observable<any> {
+    return this.firestore.collection('menu').valueChanges({ idField: 'id' });
+  }
+
+  onUpdateFoodStatus(id: string, data: { status: boolean }): void {
+    this.firestore.collection('menu').doc(id).update(data);
+  }
+
+  onCheckboxChange(id: string, event: Event): void {
+    const isChecked = (<HTMLInputElement>event.target).checked;
+    this.onUpdateFoodStatus(id, { status: isChecked });
+  }
+}
---
+++ restaurant-website/src/app/homepage/homepage.component.ts
@@ -0,0 +1,82 @@
+import { Firestore } from '@angular/fire/firestore';
+import { SocketService } from './../services/socket-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { io } from 'socket.io-client';
+import { Observable, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { FirebaseFood } from '../models/interface';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss'],
+})
+export class HomepageComponent implements OnInit {
+  private socket: any;
+  item$: Observable<FirebaseFood[]>;
+  firestore: any;
+  constructor(
+    private router: Router,
+    private socketService: SocketService,
+    private http: HttpClient,
+    private fireStore: AngularFirestore
+  ) {
+    this.socket = io('https://mbfoodsapi.azurewebsites.net/');
+    // this.socket = io('http://localhost:8000/');
+    this.item$ = this.onGetAllFoods();
+    this.item$.subscribe((res) => this.socketService.setFoodArray(res));
+  }
+
+  foodArray: any;
+  closingTime: string = '';
+  currentTime: string = '';
+  public orderStatus: boolean = false;
+  loading: boolean = true;
+  breakTime: { closingTime: string; openingTime: string } = {
+    closingTime: '',
+    openingTime: '',
+  };
+  closingTimeError = false;
+  subscription: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.http
+      .get('https://mbfoodsapi.azurewebsites.net/')
+      .subscribe((res: any) => {
+        this.orderStatus = res.orderStatus;
+        if (this.orderStatus) {
+          this.closingTimeError = true;
+        } else {
+          this.closingTimeError = false;
+        }
+      });
+
+    this.socket.on('orderStatus', (res: { orderStatus: boolean }) => {
+      this.orderStatus = res.orderStatus;
+      if (res.orderStatus) {
+        this.closingTimeError = true;
+      } else {
+        this.closingTimeError = false;
+      }
+    });
+
+    this.foodArray = this.socketService.getAllFoods();
+  }
+
+  onProceedToOrderPage(id: string): void {
+    if (this.orderStatus) {
+      this.closingTimeError = true;
+    } else {
+      this.closingTimeError = false;
+      this.router.navigate(['/orders', id]);
+    }
+  }
+
+  onGetAllFoods(): Observable<any> {
+    return this.fireStore
+      .collection('menu', (food) => food.where('status', '==', true))
+      .valueChanges({ idField: 'id' });
+  }
+}
